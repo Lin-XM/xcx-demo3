fdd602eabdcf9be60b2b7ed60a0f0204ecc9662f
xiaoming <2604863701@qq.com>
2022-08-27 16:41:18 +0800

    分类导航

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
 import App from './App'
+import {$http} from "@escook/request-miniprogram"
+
+uni.$http = $http
+$http.baseUrl = 'https://api-ugo-web.itheima.net'
+
+// 自定义方法
+uni.$showMsg = function(title='数据加载失败', duration=1000){
+	uni.showToast({
+		title,duration,
+		icon:'none'
+	})
+}
+
+// 配置拦截器
+$http.beforeRequest = function(){
+	uni.showLoading({
+		title:'数据加载中。',
+		
+	})
+}
+$http.afterRequest = function(){
+	uni.hideLoading()
+}
 
 // #ifndef VUE3
 import Vue from 'vue'
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,18 @@
 <template>
 	<view>
-		首页
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" circular="true">
+			<swiper-item v-for="(item,i) in swiperList" key="i">
+				<navigator class="swiper-item" :url="'/subpkg/goods_detail/goods_detail?goods_id='+ item.goods_id">
+					<image :src="item.image_src"></image>
+				</navigator>
+			</swiper-item>
+
+		</swiper>
+		<view class="nav-list">
+			<view class="nav-item" v-for="(item, index) in navList" :ke='index' @click="navClickHandler(item)">
+				<image :src="item.image_src" class="nav-img"></image>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,12 +20,62 @@
 	export default {
 		data() {
 			return {
-				
+				swiperList: [],
+				navList: []
 			};
+		},
+		onLoad() {
+			this.getSwiperList()
+			this.getNavList()
+		},
+		methods: {
+			async getSwiperList() {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/swiperdata')
+				if (res.meta.status !== 200) {
+					return uni.$showMsg()
+				}
+				this.swiperList = res.message
+			},
+			async getNavList() {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/catitems')
+				if (res.meta.status !== 200) return uni.$showMsg
+				this.navList = res.message
+			},
+			navClickHandler(item){
+				if(item.name === '分类'){
+					uni.switchTab({
+						url:"/pages/cate/cate"
+					})
+				}
+			}
 		}
+
 	}
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+	swiper {
+		height: 330rpx;
 
+		.swiper-item,
+		image {
+			width: 100%;
+			height: 100%;
+		}
+	}
+
+	.nav-list {
+		display: flex;
+		justify-content: space-around;
+		margin: 15px 0;
+
+		.nav-img {
+			width: 120rpx;
+			height: 140rpx;
+		}
+	}
 </style>
