dd5000ecdae480f9065695be45f8525e0adf0197
xiaoming <2604863701@qq.com>
2022-08-27 17:27:55 +0800

    floor img

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -13,6 +13,30 @@
 				<image :src="item.image_src" class="nav-img"></image>
 			</view>
 		</view>
+
+		<view class="floor-list">
+			<view class="floor-item" v-for="(item,index) in floorList" :key="index">
+				<!-- <text>{{item.floor_title.name}}</text> -->
+				<image :src="item.floor_title.image_src" class="floor-image"></image>
+
+				<view class="floor-img-box">
+					<view class="left-img-box">
+						<image :src="item.product_list[0].image_src" mode="widthFix"
+							:style="{width:item.product_list[0].image_width + 'rpx'}"></image>
+					</view>
+					<view class="right-img-box">
+						<view class="right-img-item" v-for="(item2,i2) in item.product_list"
+						:key="i2" v-if="i2 !==0">
+							<image :src="item2.image_src" mode="widthFix"
+								:style="{width:item2.image_width + 'rpx' }"></image>
+						</view>
+					</view>
+			
+				</view>
+			</view>
+
+		</view>
+
 	</view>
 </template>
 
@@ -21,12 +45,14 @@
 		data() {
 			return {
 				swiperList: [],
-				navList: []
+				navList: [],
+				floorList: []
 			};
 		},
 		onLoad() {
 			this.getSwiperList()
 			this.getNavList()
+			this.getFloorList()
 		},
 		methods: {
 			async getSwiperList() {
@@ -45,12 +71,20 @@
 				if (res.meta.status !== 200) return uni.$showMsg
 				this.navList = res.message
 			},
-			navClickHandler(item){
-				if(item.name === '分类'){
+			navClickHandler(item) {
+				// 三个 分类 秒杀 超市购 母婴
+				if (item.name === '分类') {
 					uni.switchTab({
-						url:"/pages/cate/cate"
+						url: "/pages/cate/cate"
 					})
 				}
+			},
+			async getFloorList() {
+				const {
+					data: res
+				} = await uni.$http.get("/api/public/v1/home/floordata")
+				if (res.meta.status !== 200) return uni.$showMsg
+				this.floorList = res.message
 			}
 		}
 
@@ -76,6 +110,25 @@
 		.nav-img {
 			width: 120rpx;
 			height: 140rpx;
+		}
+	}
+
+	.floor-list {
+		.floor-item {
+			.floor-image {
+				width: 100%;
+				height: 60rpx;
+			}
+			.floor-img-box{
+				display:flex;
+				padding-left: 10rpx;
+				.right-img-box{
+					display:flex;
+					flex-wrap: wrap;
+					justify-content: space-around;
+				}
+			}
+
 		}
 	}
 </style>
